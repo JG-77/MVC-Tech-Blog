e59db26eaf5a30dae8b433df0d59085610548cf2
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-11 18:23:10 -0700

    added comments to blog-routes.js, and created views files for html structure set up

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 
+// sign up to create new user
+
+// Login
+
+//Logout
+
 module.exports = router;
--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+//get home page with all blog posts
 router.get('/', async (req, res) => {
   try {
     res.render('home');
@@ -8,4 +9,21 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// get dashboard where user's post display
+
+// get new post template
+
+// POST new blog post
+
+//get specific user post
+
+// PUT --> update a saved blog post
+
+// DELETE --> delete user's blog post
+
+// get other user's existing post
+
+//POST comment on blog post
+
 module.exports = router;
