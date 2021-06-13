2fa1cb5af23ba69688cef61c2afc45dc812ab653
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-13 12:20:40 -0700

    set up Post belongstoMany relationship

--- models/Index.js
+++ models/Index.js
@@ -3,29 +3,21 @@ const User = require('./User');
 const Comment = require('./Comment');
 const PostComment = require('./PostComment');
 
-// A user can have many posts
-// User.hasMany(Post, {
-//   foreignKey: 'user_id',
-// });
-
 // A post belongs to a single user
 Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-// A user can have many comments
-// User.hasMany(Comment, {
-//   through: {
-//     model: PostComment,
-//     //unique: false,
-//   },
-// });
-
 // A comment belongs to a single user
 Comment.belongsTo(User, {
   through: {
     model: PostComment,
-    //unique: false,
+  },
+});
+
+Post.belongsToMany(Comment, {
+  through: {
+    model: PostComment,
   },
 });
 
