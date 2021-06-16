a7b483d82bf0e5261a0db93a3cc72f6af3bb975b
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-15 22:16:24 -0700

    debugged dashboard route

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -53,7 +53,7 @@ router.get('/dashboard', checkAuthentication, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await Post.findAll({
-      where: {user_id = req.session.id},
+      where: { user_id: req.session.id },
     });
 
     const user = userData.map((post) => post.get({ plain: true }));
